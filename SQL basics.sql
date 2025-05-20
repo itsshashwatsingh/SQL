4366f968a5d41f0307d13d266e8b13c476e56fcd
itsshashwatsingh <shashwat98k@gmail.com>
2025-05-21 00:37:31 +0530

    aggrigation function

--- SQL basics.sql
+++ SQL basics.sql
@@ -10,7 +10,7 @@ create table products
 	released_date	date
 );
 
--- viewing table
+-- viewing table 
 
 select * from products;
 
@@ -18,7 +18,8 @@ select * from products;
 
 insert into products (product_code,product_name,price,released_date)
 values(001,'Iphone 15 pro max',999.5,to_date('22-04-2023','dd-mm-yyyy'));
-
+                              -- to_date function--
+							  
 insert into products (product_code,product_name,price,released_date)
 values(002,'Iphone 15 pro',599.5,to_date('21-04-2023','dd-mm-yyyy'));
 
@@ -28,6 +29,7 @@ values(003,'macbook pro',1499.5,to_date('22-07-2022','dd-mm-yyyy'));
 insert into products (product_code,product_name,price,released_date)
 values(004,'Ipad pro max',899.5,to_date('27-01-2023','dd-mm-yyyy'));
 
+-- FILTER QUERIES -> use 'where' and add condition such as price > 1000 
 
 -- to get the data from products where price is greater than 1000
 
@@ -36,6 +38,57 @@ where price > 1000  --only got the data of macbook pro since price is greater th
 
 --to get only one column from products where price is greater than 1000
 
-   --name of the column--
+  --name of the column--
 select product_name from products
 where price > 1000 
+
+-- now the same can be done with dates lets try
+
+-- 'to_char' FUNCTION and 'extract' FUNCTION
+
+select * from products 
+where released_date = '2023' -- no this will throw an error because released_date and '2022'
+--are of diffent datatypes to fix this we will use 'to_char function' to convert date to char
+
+select * from products 
+where to_char(released_date,'yyyy')='2022'
+
+-- can perform the same filtering using 'extract function' in our query
+
+select * from products
+where extract(year from released_date) = '2022'
+  --extract function--
+
+--  ALIAS (disple column with different name) -> use as 
+--(to display a column with different name while applying filter query)
+
+select product_name as xyproduct from products 
+where price > 1000
+
+-- AGGRIGATION FUNCTION
+
+-- COUNT FUNCTION
+
+-- to display total number of records or rows entered we can use count(*) function 
+
+select count(*) from products -- 3 , got count of total rows entered
+
+-- we can also use count with filter
+
+select count(*) from products -- 1 , got count from products with price >1000
+where price > 1000
+
+-- SUM FUNCTION -> sum(column name of which you want total sum)
+
+-- we can also find the sum of values in a float or int column using 'sum() function'
+
+select sum(price) from products -- 3998 , got total price
+
+--AVG FUNCTION -> avg(column name of which you want average)
+
+-- similarly , we can also find average using avg function
+
+select avg(price) from products -- 999.5, got average of price
+
+
+drop table products 
