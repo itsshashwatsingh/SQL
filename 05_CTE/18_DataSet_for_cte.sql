bebc41443aad3f1499204f5a97cbbf2aac16a7f2
itsshashwatsingh <shashwat98k@gmail.com>
2025-05-27 16:40:15 +0530

    'sql_tutorial'

---
+++ 05_CTE/18_DataSet_for_cte.sql
@@ -0,0 +1,50 @@
+
+-- QUERY 1 :
+
+create table emp
+( emp_ID int
+, emp_NAME varchar(50)
+, SALARY int);
+
+insert into emp values(101, 'Mohan', 40000);
+insert into emp values(102, 'James', 50000);
+insert into emp values(103, 'Robin', 60000);
+insert into emp values(104, 'Carol', 70000);
+insert into emp values(105, 'Alice', 80000);
+insert into emp values(106, 'Jimmy', 90000);
+
+
+
+
+
+
+-- QUERY 2 :
+
+create table sales
+(
+	store_id  		int,
+	store_name  	varchar(50),
+	product			varchar(50),
+	quantity		int,
+	cost			int
+);
+insert into sales values
+(1, 'Apple Originals 1','iPhone 12 Pro', 1, 1000),
+(1, 'Apple Originals 1','MacBook pro 13', 3, 2000),
+(1, 'Apple Originals 1','AirPods Pro', 2, 280),
+(2, 'Apple Originals 2','iPhone 12 Pro', 2, 1000),
+(3, 'Apple Originals 3','iPhone 12 Pro', 1, 1000),
+(3, 'Apple Originals 3','MacBook pro 13', 1, 2000),
+(3, 'Apple Originals 3','MacBook Air', 4, 1100),
+(3, 'Apple Originals 3','iPhone 12', 2, 1000),
+(3, 'Apple Originals 3','AirPods Pro', 3, 280),
+(4, 'Apple Originals 4','iPhone 12 Pro', 2, 1000),
+(4, 'Apple Originals 4','MacBook pro 13', 1, 2500);
+
+select * from sales;
+select * from emp;
+
+
+
+drop table emp;
+DrOP table sales ;
